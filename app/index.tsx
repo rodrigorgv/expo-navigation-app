f56dbbe26d7d29ce12b487401b85c86d32caacf8
rodrigorgv <rodrigo.emilianogv@gmail.com>
2025-08-31 19:13:21 -0600

    initial commit. tailwind was configured to set styles on index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View className='mt-10'>
+      <Text className='text-3xl font-bold text-blue-500'>Hola Mundo</Text>
+    </View>
+  )
+}
+
+export default index
